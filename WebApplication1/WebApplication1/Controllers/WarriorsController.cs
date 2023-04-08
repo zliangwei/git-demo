f736366455a03b496d5d21a40f8c0dc222729f3e
“kunanshen” <forthree@curry.com>
2023-04-08 14:38:35 +0800

    库男神提交测试

---
+++ WebApplication1/WebApplication1/Controllers/WarriorsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WarriorsController : ControllerBase
+    {
+        //我是库男神，这是我的新项目
+    }
+}
